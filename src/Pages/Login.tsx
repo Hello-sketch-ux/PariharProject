bfe3ac64b4cb4799e3118eed2226e26c0ab492fb
MohitMorya2 <mohitmorya56@gmail.com>
2025-04-18 17:55:13 +0530

    login api checked

--- src/Pages/Login.tsx
+++ src/Pages/Login.tsx
@@ -27,7 +27,7 @@ const Login: React.FC<LoginProps> = ({ onLogin }) => {
     }
 
     try {
-      const response = await fetch('http://localhost:5000/api/auth/login', {
+      const response = await fetch('http://192.168.1.5:5000/api/auth/login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ firstName, lastName, email, mobile, password }),
