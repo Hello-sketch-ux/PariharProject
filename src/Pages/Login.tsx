12f5482a9830d191a96d9acad78ecc69792e1115
MohitMorya2 <mohitmorya56@gmail.com>
2025-04-18 21:32:00 +0530

    login checked

--- src/Pages/Login.tsx
+++ src/Pages/Login.tsx
@@ -27,7 +27,7 @@ const Login: React.FC<LoginProps> = ({ onLogin }) => {
     }
 
     try {
-      const response = await fetch('https://parihar-project.vercel.app/api/auth/login', {
+      const response = await fetch('https://www.pariharindia.com/api/auth/login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ firstName, lastName, email, mobile, password }),
