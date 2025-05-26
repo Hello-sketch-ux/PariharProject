0dc5efd631bfc940a9dd42ff25f1d03a5eac28b4
MohitMorya2 <mohitmorya56@gmail.com>
2025-05-26 17:25:43 +0530

    host change

--- Backend/server.js
+++ Backend/server.js
@@ -16,16 +16,10 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 
-const allowedOrigins = ['http://localhost:3000', 'https://www.pariharindia.com', "https://parihar-project.vercel.app/"];
+const allowedOrigins = ['http://localhost:5173', 'https://www.pariharindia.com', "https://parihar-project.vercel.app/"];
 
 app.use(cors({
-  origin: function(origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
+  origin: allowedOrigins,
   credentials: true
 }));
 
